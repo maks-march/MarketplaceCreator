1c8e4e2874f57f27cd3f88b9d00d34a5f8acd15c
marchenko <1maks_2055@mail.ru>
2025-12-04 11:40:47 +0500

    some fix

--- backend/DataAccess/Extensions.cs
+++ backend/DataAccess/Extensions.cs
@@ -13,10 +13,7 @@ public static class Extensions
         serviceCollection.AddScoped<IBrandRepository, BrandRepository>();
         serviceCollection.AddDbContext<AppContext>(x =>
         {
-            // для локального пользования
-             //x.UseNpgsql("Host=localhost;Port=5432;Database=MainDB;Username=postgres;Password=123456;");
-            // для docker
-            x.UseNpgsql("Host=postgres;Port=5432;Database=MainDB;Username=postgres;Password=123456;");
+            x.UseNpgsql("Host=localhost;Port=5432;Database=MainDB;Username=postgres;Password=123456;");
         });
         
         return serviceCollection;
