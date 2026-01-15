c83bc49a9117e93afad3e0ab5aaf3f91b2819bd6
maks march <1maks_2055@mail.ru>
2026-01-15 15:17:44 +0500

    блиа новости

--- backend/Shared/DataTransferObjects/Request/NewsDto/NewsCreateDto.cs
+++ backend/Shared/DataTransferObjects/Request/NewsDto/NewsCreateDto.cs
@@ -9,7 +9,6 @@ public class NewsCreateDto : BaseDto
     [StringLength(50, MinimumLength = 3, ErrorMessage = "Имя от 3 до 50 символов")]
     public string? Title { get; set; }
     
-    [StringLength(2000, MinimumLength = 5, ErrorMessage = "Описание до 2000 символов")]
     public string Description { get; set; } = string.Empty;
     
     [Required(ErrorMessage = "Указание бренда обязательно")]
--- backend/Shared/DataTransferObjects/Request/NewsDto/NewsUpdateDto.cs
+++ backend/Shared/DataTransferObjects/Request/NewsDto/NewsUpdateDto.cs
@@ -7,6 +7,5 @@ public class NewsUpdateDto : BaseDto
     [StringLength(50, MinimumLength = 3, ErrorMessage = "Имя от 3 до 50 символов")]
     public string? Title { get; set; } = null;
     
-    [StringLength(2000, ErrorMessage = "Описание до 2000 символов")]
     public string? Description { get; set; } = null;
 }
